bb0796bc894458a9188abb4289dd58841bcb3759
Minatsu.Homma <minatsu.homma@gmail.com>
2022-04-05 15:50:33 +0900

    20220405-15:50

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,3 +1,10 @@
-$(document)(()=>{
+var elInner, duration;
+elInner = document.getElementsByClassName('p1-container');
+duration = 10;
     
-})
+if(elInner.length) {
+    setTimeout(function () {
+        console.log('booboo')
+        window.location.href = '/fjord_2';
+    }, duration*1000)
+};
